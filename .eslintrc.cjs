9896b518ae9420ba3e0e7b9c226dfc0a8ed6c20e
Kevin Wilson <kevin.wilson@thisisbud.com>
2026-04-29 16:49:24 +0100

    chore: initialise monorepo skeleton

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    curly: ["error", "all"],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+  },
+  ignorePatterns: ["dist/**", ".astro/**", "node_modules/**", "**/*.cjs"],
+};
